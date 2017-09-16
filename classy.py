a811dea46bfe7efb48f0f9c8669ce1734b08d1ce
Tuskayn <tuskayn@gmail.com>
2017-09-16 17:29:09 +0200

    Step 3 & 4
    Ajout de:
    -Gestion des items
    -Garde
    
    Macgyver doit maintenant ramasser les 3 objets pour endormir le garde et sortir pour gagner.

--- classy.py
+++ classy.py
@@ -4,16 +4,18 @@ Class for MacGyver Maze
 
 
 import pygame
-from pygame.locals import *
+import random
 
 
 class Maping:
-    """Method to a"""
+    """Class to create the map"""
     def __init__(self):
+        """Initial settings for the class"""
         self.file = "maps/map1.txt"
-        self.level = 0
+        self.map = ''
 
     def generate(self):
+        """Generate a map from a file thru list"""
         with open(self.file, 'r') as file:
             level_map = []
             for line in file:
@@ -22,61 +24,98 @@ class Maping:
                     if sprite != '\n':
                         line_level.append(sprite)
                 level_map.append(line_level)
-            self.level = level_map
+            self.map = level_map
 
     def display(self, window):
+        """Display the map from the generation"""
         wall = pygame.image.load("img/wall.png").convert_alpha()
         flor = pygame.image.load("img/flor40.jpg").convert()
         num_line = 0
-        for line in self.level:
+        for line in self.map:
             num_case = 0
             for sprite in line:
                 x = num_case * 40
                 y = num_line * 40
                 if sprite == '1':
                     window.blit(wall, (x, y))
-                elif sprite == '0' :
+                else:
                     window.blit(flor, (x, y))
-                num_case +=1
-            num_line +=1
+                num_case += 1
+            num_line += 1
 
 
 class Characters:
     """Class to create a character"""
     def __init__(self, cx, cy, path, level):
+        """Initial settings for the class"""
         self.items = 0
         self.health = 1
         self.case_x = cx
         self.case_y = cy
         self.x = self.case_x * 40
-        self.y = self.case_x * 40
+        self.y = self.case_y * 40
         self.level = level
         self.sprite = pygame.image.load(path).convert_alpha()
 
-    def alive(self):
-        return self.health > 0
+    def chgsprite(self, path):
+        self.sprite = pygame.image.load(path).convert_alpha()
 
     def move(self, direction):
+        """Methode to move the hero"""
         if direction == 'right':
             if self.case_x < 16:
-                if self.level.level[self.case_y][self.case_x+1] != '1':
+                if self.level.map[self.case_y][self.case_x+1] != '1':
                     self.case_x += 1
                     self.x = self.case_x * 40
         if direction == 'left':
             if self.case_x > 0:
-                if self.level.level[self.case_y][self.case_x-1] != '1':
+                if self.level.map[self.case_y][self.case_x-1] != '1':
                     self.case_x -= 1
                     self.x = self.case_x * 40
         if direction == 'up':
             if self.case_y > 0:
-                if self.level.level[self.case_y-1][self.case_x] != '1':
+                if self.level.map[self.case_y-1][self.case_x] != '1':
                     self.case_y -= 1
                     self.y = self.case_y * 40
         if direction == 'down':
             if self.case_y < 16:
-                if self.level.level[self.case_y+1][self.case_x] != '1':
+                if self.level.map[self.case_y+1][self.case_x] != '1':
                     self.case_y += 1
                     self.y = self.case_y * 40
 
-    def getpos(self):
-        return self.position
+    def getitem(self):
+        self.items += 1
+
+    def damage(self):
+        self.health -= 1
+
+
+class Items:
+    """Class to create an item"""
+    def __init__(self, name,  path, level):
+        """Initial settings for the item"""
+        self.id = name
+        self.health = 1
+        self.level = level
+        self.case_x, self.case_y = self.randpos()
+        self.x = self.case_x * 40
+        self.y = self.case_y * 40
+        self.sprite = pygame.image.load(path).convert_alpha()
+
+
+    def randpos(self):
+        while True:
+            self.case_x = random.randrange(1, 16)
+            self.case_y = random.randrange(1, 16)
+            if self.level.map[self.case_y][self.case_x] == '0':
+                self.level.map[self.case_y][self.case_x] = self.id
+                break
+        return self.case_x, self.case_y
+
+    def damage(self):
+        self.health -= 1
+
+    def display(self, window):
+        """Display the item on screen"""
+        if self.health > 0:
+            window.blit(self.sprite, (self.x, self.y))
--- macgyver.py
+++ macgyver.py
@@ -3,31 +3,39 @@ MacGyver Maze
 WIN : Reach the 3 artifacts to send to sleep the guard and exit
 """
 
-import pygame
+
 from classy import *
 from pygame.locals import *
 
 
+# Init
 pygame.init()
 window = pygame.display.set_mode((680, 680))
 icone = pygame.image.load("img/mac.png").convert_alpha()
 pygame.display.set_icon(icone)
 pygame.display.set_caption("MacGyver")
 
-
-#Init Generate Display of the level
-
-
+# Init Generate Display of the level
 Level = Maping()
 Level.generate()
 Level.display(window)
 
+# Creation of MacGyver
+Mac = Characters(1, 1, "img/mac.png", Level)
 
-#Creation of MacGyver
+# Creation of the Guard
+Keeper = Characters(15, 14, "img/keeper.png", Level)
 
-
-Mac = Characters(2, 2, "img/mac.png", Level)
-
+# Creation of items / Edition of Level.map
+Needle = Items("N", "img/needle.png", Level)
+Needle.display(window)
+Level.map[Needle.case_y][Needle.case_x] = Needle.id
+Ether = Items("E", "img/ether.png", Level)
+Ether.display(window)
+Level.map[Ether.case_y][Ether.case_x] = Ether.id
+Tube = Items("T", "img/tube.png", Level)
+Tube.display(window)
+Level.map[Tube.case_y][Tube.case_x] = Tube.id
 
 continue_game = 1
 while continue_game:
@@ -44,8 +52,53 @@ while continue_game:
                 Mac.move('down')
             if event.key == K_RIGHT:
                 Mac.move('right')
+
     Level.display(window)
-    window.blit(Mac.sprite, (Mac.x, Mac.y))
+    Needle.display(window)
+    Ether.display(window)
+    Tube.display(window)
+
+    if Keeper.health > 0:
+        window.blit(Keeper.sprite, (Keeper.x, Keeper.y))
+    else:
+        Keeper.chgsprite("img/rip.png")
+        window.blit(Keeper.sprite, (Keeper.x, Keeper.y))
+    if Mac.health > 0:
+        window.blit(Mac.sprite, (Mac.x, Mac.y))
+    else:
+        Mac.chgsprite("img/rip.png")
+        window.blit(Mac.sprite, (Mac.x, Mac.y))
+
+    # Items gestion
+    if Level.map[Mac.case_y][Mac.case_x] == 'N':
+        Needle.damage()
+        Level.map[Mac.case_y][Mac.case_x] = '0'
+        Mac.getitem()
+    if Level.map[Mac.case_y][Mac.case_x] == 'E':
+        Ether.damage()
+        Level.map[Mac.case_y][Mac.case_x] = '0'
+        Mac.getitem()
+    if Level.map[Mac.case_y][Mac.case_x] == 'T':
+        Tube.damage()
+        Level.map[Mac.case_y][Mac.case_x] = '0'
+        Mac.getitem()
+
+    # Fight
+    if Level.map[Mac.case_y][Mac.case_x] == 'K':
+        if Mac.items >= 3:
+            Keeper.damage()
+            Level.map[Mac.case_y][Mac.case_x] = '0'
+
+        else:
+            continue_game = 0
+            print("GAME OVER")
+
+    # WIN
+    if Level.map[Mac.case_y][Mac.case_x] == 'A':
+        if Mac.items >= 3:
+            continue_game = 0
+            print("WIN")
+
+    # Refresh
     pygame.display.flip()
-    if Level.level[Mac.case_y][Mac.case_x] == 'A':
-        continue_game = 0
+
